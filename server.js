0de44fa24d2de6fba13304ce855d627c1708a261
memoizedk <dadwalkhushi05@gmail.com>
2025-09-24 01:54:42 +0530

    middleware

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 const PORT = process.env.PORT || 3500;
 
-app.get("/", (req, res) => {
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get(["/", "/index", "/index.html"], (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
-app.get("/index.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-app.get("/new-page", (req, res) => {
+
+app.get(["/new-page", "/new-page.html"], (req, res) => {
   res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
-app.get("/new-page.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+
+const one = (req, res, next) => { console.log("one"); next(); };
+const two = (req, res, next) => { console.log("two"); next(); };
+const three = (req, res) => { console.log("three"); res.send("Finished!"); };
+
+app.get(["/chain", "/chain.html"], [one, two, three]);
+
+// 404
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
