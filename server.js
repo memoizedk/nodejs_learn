1a7c34a66237a229fec829af4917f9bec5880265
memoizedk <dadwalkhushi05@gmail.com>
2025-10-07 13:07:08 +0530

    employee route

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.use('/subdir',express.static(path.join(__dirname, "/public")));
 
 app.use('/',require('./routes/root'));
 app.use('/subdir',require('./routes/subdir'));
+app.use('/employees',require('./routes/api/employees'));
 
 // 404
 app.use((req, res) => {
