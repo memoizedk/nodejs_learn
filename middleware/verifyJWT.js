da384a909599d8539a0361ea267108b201bb9c2d
memoizedk <dadwalkhushi05@gmail.com>
2025-10-27 13:20:53 +0530

    authorise user roles and permissions

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,14 +2,16 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const verify = (req, res, next) => {
-  const authHeader = req.header("authorization");
-  if (!authHeader) return res.sendStatus(401);
-  console.log(authHeader); //bearer token
+  const authHeader = req.headers?.authorization || req.headers?.Authorization;
+  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
+
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403); //invalid token
-    req.user = decoded.username;
+    if (err) return res.sendStatus(403); // invalid token
+    req.user = decoded?.UserInfo?.username;
+    req.roles = decoded?.UserInfo?.roles;
     next();
   });
 };
+
 module.exports = verify;
