cd21f4be7a6a77031cb16f8984a41cb3c27c89a7
memoizedk <dadwalkhushi05@gmail.com>
2025-11-05 11:34:31 +0530

    connecting to mongoDB

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
 const verify = (req, res, next) => {
   const authHeader = req.headers?.authorization || req.headers?.Authorization;
